2b9c41c65ed2e2f3b607b43d1772d99ea741f2cd
halegintaga <fozilovahilola226@gmail.com>
2024-04-30 13:23:58 +0300

    change app

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
 
   return (
     <div>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate sapiente repellat sunt, numquam sit, autem a ipsam atque, assumenda delectus maiores praesentium tempore quam voluptate magnam enim dolores blanditiis nobis!</p>
       <Condishinal/>
     </div>
   )
